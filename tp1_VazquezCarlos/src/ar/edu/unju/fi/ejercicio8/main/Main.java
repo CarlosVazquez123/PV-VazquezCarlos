94952c8be32e49cee1c15b70099f52175f1e0c69
carlo <carlo@Carlitoss.Carlitoss>
2024-04-12 23:13:32 -0300

    Ejercicio 8 Tp1

---
+++ tp1_VazquezCarlos/src/ar/edu/unju/fi/ejercicio8/main/Main.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.ejercicio8.main;
+
+import java.util.Scanner;
+
+import ar.edu.unju.fi.ejercicio8.model.CalculadoraEspecial;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+
+		CalculadoraEspecial calculadora = new CalculadoraEspecial();
+		
+		System.out.println("Ingrese numero: ");
+		
+		calculadora.setN(teclado.nextInt());
+	
+		System.out.println("Sumatoria: "+calculadora.calcularSumatoria());
+		
+		
+		System.out.println("Productoria: "+calculadora.calcularProductoria());
+	}
+
+}
